5bc4948cd62bca92479d830ff21238a79cc0aee8
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2019-03-05 17:31:15 -0800

    some merge sort validation

--- src/app/views/mergesort/mergesort.component.ts
+++ src/app/views/mergesort/mergesort.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-mergesort',
@@ -6,13 +6,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mergesort.component.css']
 })
 export class MergesortComponent implements OnInit {
-  sortArray;
+  int_array = [];
+  userText;
+  input_error: String;
 
   constructor() {
-    this.sortArray = [5, 3, 10, 12, 7, 1, 18];
+    this.userText = "";
+    this.input_error = "";
   }
 
   ngOnInit() {
+  }
+
+  convertStringToArray() {
+    this.int_array = this.userText.trim().split(" ");
+  }
+
+  validateInput() {
+    let count = 0;
+    this.userText.split("").forEach(ch => {
+      if(!(isNaN(ch) && ch != " ")) {
+        count++;
+      }
+    });
+    if(count == this.userText.length){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  sortArray() {
+    let valid = this.validateInput();
+    console.log("end: " + valid);
+    if(valid) {
+      this.convertStringToArray();
+    } else {
+      this.input_error = "Error in input."
+    }
+  }
+
+  clearInputError() {
+    this.input_error = "";
   }
 
 }
