1abe7885ab7d27a2588c11b56ab9a4f9d593a539
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2019-03-09 17:45:01 -0800

    some validation and attempt to display array splits

--- src/app/views/mergesort/mergesort.component.ts
+++ src/app/views/mergesort/mergesort.component.ts
@@ -19,7 +19,17 @@ export class MergesortComponent implements OnInit {
   }
 
   convertStringToArray() {
-    this.int_array = this.userText.trim().split(" ");
+    let valid = this.validateInput();
+    if(valid) {
+      if(this.userText.trim() == "") {
+        this.int_array = [];
+      } else {
+        this.int_array = this.userText.trim().split(" ");
+      }
+    } else {
+      this.input_error = "Error in input."
+      this.int_array = [];
+    }
   }
 
   validateInput() {
@@ -36,18 +46,41 @@ export class MergesortComponent implements OnInit {
     }
   }
 
+  separateNumbers(arr, i) {
+    if(arr.length != 1) {
+      let left = arr.splice(0, arr.length/2);
+      let right = arr.splice(arr.length/2 - 1, arr.length);
+
+      console.log("left: " + left);
+      console.log("right: " + right);
+
+      console.log("left.length: " + left.length);
+      console.log("i: " + i);
+
+      setTimeout(function () {
+        console.log("left recursion: " + (i+left.length-1));
+        document.getElementById((i+left.length-1) + "").style.paddingLeft = '5px';
+        this.separateNumbers(left, i);
+      }, 2000);
+
+      setTimeout(function () {
+        console.log("right recursion: " + (i+left.length));
+        document.getElementById((i+left.length) + "").style.paddingRight = '5px';
+        this.separateNumbers(right, i+left.length);
+      }, 2000);
+    }
+  }
+
   sortArray() {
-    let valid = this.validateInput();
-    console.log("end: " + valid);
-    if(valid) {
+    if(this.input_error.length == 0) {
       this.convertStringToArray();
-    } else {
-      this.input_error = "Error in input."
+      setTimeout(function () {
+        this.separateNumbers(this.int_array, 0);
+      }, 2000);
     }
   }
 
   clearInputError() {
     this.input_error = "";
   }
-
 }
