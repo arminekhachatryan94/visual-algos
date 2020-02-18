0aa7dac54c46eb63c58c6f102fda8fef842e1ac2
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2020-02-17 21:24:39 -0800

    add example videos to home page

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,15 +6,9 @@ import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  algo: string;
   constructor() { }
 
   ngOnInit() {
-    this.algo = 'counting-inversions';
-  }
-
-  changeAlgo(name: string) {
-    this.algo = name;
-    console.log(this.algo);
+    ;
   }
 }
